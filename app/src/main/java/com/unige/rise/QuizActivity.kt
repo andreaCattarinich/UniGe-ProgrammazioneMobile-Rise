dc2387c2b91d9773eea6cc788057bfa73c8581ea
Andrea Cattarinich <andy.cattarinich@gmail.com>
2024-07-11 10:19:06 +0200

    Quiz results (layout and code)

--- app/src/main/java/com/unige/rise/QuizActivity.kt
+++ app/src/main/java/com/unige/rise/QuizActivity.kt
@@ -1,7 +1,9 @@
 package com.unige.rise
 
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -9,6 +11,7 @@ import androidx.databinding.DataBindingUtil
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.unige.rise.databinding.ActivityQuizBinding
+import kotlin.math.roundToInt
 
 class QuizActivity : AppCompatActivity() {
 
@@ -37,7 +40,7 @@ class QuizActivity : AppCompatActivity() {
 
                 //Toast.makeText(this, viewModel.arrayQuestionSize.value.toString(), Toast.LENGTH_SHORT).show()
                 //Toast.makeText(this, viewModel.currentQuestionIndex.value.toString(), Toast.LENGTH_SHORT).show()
-                var question = dataSnapshot
+                val question = dataSnapshot
                     .child("quiz")
                     .child("1")
                     .child("question")
@@ -46,18 +49,18 @@ class QuizActivity : AppCompatActivity() {
             }
 
         binding.trueBtn.setOnClickListener {
-            var nextQuestion = viewModel.answer(true)
-            if(nextQuestion != -1 && nextQuestion!! <= 2) {
-                loadNextQuestion(nextQuestion!!)
+            val nextQuestion = viewModel.answer(true)
+            if(nextQuestion <= viewModel.getArrayQuestionSize()!!) {
+                loadNextQuestion(nextQuestion)
             } else {
                 getQuizResult()
             }
         }
 
         binding.falseBtn.setOnClickListener {
-            var nextQuestion = viewModel.answer(false)
-            if(nextQuestion != -1 && nextQuestion!! <= 2) {
-                loadNextQuestion(nextQuestion!!)
+            val nextQuestion = viewModel.answer(false)
+            if(nextQuestion <= viewModel.getArrayQuestionSize()!!) {
+                loadNextQuestion(nextQuestion)
             } else {
                 getQuizResult()
             }
@@ -72,7 +75,7 @@ class QuizActivity : AppCompatActivity() {
             .addOnSuccessListener { dataSnapshot ->
 
                 //Toast.makeText(this, viewModel.currentQuestionIndex.value.toString(), Toast.LENGTH_SHORT).show()
-                var question = dataSnapshot
+                val question = dataSnapshot
                     .child("quiz")
                     .child(nextQuestionIndex.toString())
                     .child("question")
@@ -83,25 +86,39 @@ class QuizActivity : AppCompatActivity() {
     }
 
     private fun getQuizResult() {
+
+        binding.trueBtn.visibility = View.GONE
+        binding.falseBtn.visibility = View.GONE
+
         var count = 0
         db.child("courses")
             .child(courseId)
             .get()
             .addOnSuccessListener { dataSnapshot ->
-                for (i in 1 until 3) {
+                for (i in 1 until (viewModel.getArrayQuestionSize()!! + 1)) {
 
-                    var correct_answer = dataSnapshot
+                    val correctAnswer = dataSnapshot
                         .child("quiz")
                         .child(i.toString())
                         .child("response")
                         .getValue(Boolean::class.java)
 
-                    var user_answer = viewModel.getUserAnswer(i)
-                    if (user_answer == correct_answer) {
+                    val userAnswer = viewModel.getUserAnswer(i)
+                    if (userAnswer == correctAnswer) {
                         count++
                     }
                 }
-                Toast.makeText(this, "Correct: $count", Toast.LENGTH_SHORT).show()
+
+                val score = (100*(count.toFloat() / viewModel.getArrayQuestionSize()!!)).roundToInt()
+                val wrong = viewModel.getArrayQuestionSize()!! - count
+                binding.question.text = "Correct: $count\nWrong: $wrong\nYou're score is $score%"
+                Toast.makeText(this, "Great Job!", Toast.LENGTH_SHORT).show()
+
+                binding.homeBtn.visibility = View.VISIBLE
+                binding.homeBtn.setOnClickListener {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                }
             }
     }
 }
